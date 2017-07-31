6e4def6b9c9aeb8cf4d3705ca71a02f7503bbd96
madhu121 <mmks1211@gmail.com>
2017-08-01 00:50:41 +0530

    first git game

---
+++ JumpandCollect/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public Rigidbody rb;
+	private Vector3 left ;
+	private Vector3 right ;
+	private Vector3 up ;
+	private Vector3 down ;
+	private Vector3 zero;
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		left = new Vector3 (-20, 0, 0);
+		right = new Vector3 (20, 0, 0);
+		up = new Vector3 (0, 0, 20);
+		down = new Vector3 (0, 0, -20);
+		zero = new Vector3 (0, 0, 0);
+		count = 0;
+	}
+	void Update ()
+	{
+		if (count == 6) {
+			Debug.Log ("You won!!");
+		}
+	}
+	 public void leftmove ()
+	{
+		rb.AddForce (left);
+	}
+	public void rightmove ()
+	{
+		rb.AddForce (right);
+	}
+	public void upmove ()
+	{
+		rb.AddForce (up);
+	}
+	public void downmove ()
+	{
+		rb.AddForce (down);
+	}
+	public void zerovelocity ()
+	{
+		rb.AddForce (zero);
+	}
+	public void jump ()
+	{
+		rb.AddForce (new Vector3 (0, 600, 0));
+			}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("p")) 
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+		}
+	}
+}
---
+++ JumpandCollect/Assets/Scripts/UpandDownMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpandDownMovement : MonoBehaviour {
+	public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y <6.01) {
+			rb.AddForce (new Vector3 (0f, 25f, 0f));
+		}
+		else if(transform.position.y >8)
+		{
+			rb.AddForce (new Vector3(0f, -25f, 0f));
+				}
+	}
+}
